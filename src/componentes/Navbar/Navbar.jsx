5f59c742532d3b04956fef7ea903d585ede9e405
Soledad Shukuya <soledadshukuya@hotmail.com>
2024-01-14 21:14:51 -0300

    New navbar

--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import './navbar.css';
 
 function Navbar() {
-  return (
-      <>
-        <nav>
-            <h1 className='navbar-link'>Tuki</h1>
-        
-            <ul className='navbar-links'>
-                <li className='navbar-link'>About us</li>
-                <li className='navbar-link'>Github</li>
-            </ul>            
-        </nav>
-      </>
-  )
+    return (
+        <>
+            <nav>
+                <div className="nav-container">
+                    <h1>Tuki</h1>
+
+                    <ul className='navbar-links'>
+                        <li>
+                            <a href="#">About us</a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/solshk/cac-23644-flight-ticket" rel="noopener noreferrer" className="active">Github</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
 }
 
 export default Navbar
